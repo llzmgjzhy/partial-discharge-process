1e64023631c30fa2ce683ab390b3ed02ef19836b
llzmgjzhy <963488649@qq.com>
2024-09-14 19:22:15 +0800

    try different config

--- custom_pd_embedding/train/train_vit16_custom_embedding.py
+++ custom_pd_embedding/train/train_vit16_custom_embedding.py
@@ -93,7 +93,7 @@ def getArgparse():
     parser.add_argument(
         "--trace-steps",
         type=int,
-        default=4,
+        default=1,
         help="The num of backtracking images",
     )
     parser.add_argument(
@@ -162,6 +162,10 @@ def train(args, model, trainLoader, testLoader):
             f"Epoch : {epoch+1} - loss : {epoch_loss:.4f} - acc: {epoch_accuracy:.4f} - val_loss : {epoch_val_loss:.4f} - val_acc: {epoch_val_accuracy:.4f}\n"
         )
 
+    # save model dict
+    model_save_path = PROJECT_ROOT / "custom_pd_embedding/model/vit/vit_base"
+    torch.save(model.state_dict(), model_save_path / f"vit16_{time_now}.pth")
+
 
 if __name__ == "__main__":
     # argparse and save config to json
@@ -206,7 +210,7 @@ if __name__ == "__main__":
     # 📌:the model config decide the input dims,which is related to the data processing,so if change model config,data processing must change-->mainly is the TRANSFORMER_INPUT_DIM
     # image_size set 48,meaning the input can be divided into 9 patches,match the data processing:turn 9 prpd images into 1 prpd time sequence data
     vitModel = ViT(
-        image_size=32,
+        image_size=16,
         patch_size=16,
         num_classes=6,
         dim=args.dim,
